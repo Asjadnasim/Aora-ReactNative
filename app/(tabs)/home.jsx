8113db01806e31991821f9747c8b521ff9df5f7b
Asjad Nasim <asjadnasim@gmail.com>
2024-05-03 22:36:10 +0530

    Home Screen Videos Fetching

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,5 +1,12 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
-import React, { useState } from 'react';
+import {
+	View,
+	Text,
+	FlatList,
+	Image,
+	RefreshControl,
+	Alert,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
@@ -7,24 +14,32 @@ import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
+import VideoCard from '../../components/VideoCard';
+import { getAllPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
 
 const Home = () => {
+	const { data: posts, refetch } = useAppwrite(getAllPosts);
+
 	const [refreshing, setRefreshing] = useState(false);
+
+	// console.log(posts);
 
 	const onRefresh = async () => {
 		setRefreshing(true);
 		//re call videos -> if any new  videos appeared
+
+		await refetch();
+
 		setRefreshing(false);
 	};
 
 	return (
 		<SafeAreaView className='bg-primary h-full'>
 			<FlatList
-				data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+				data={posts}
 				keyExtractor={(item) => item.$id}
-				renderItem={({ item }) => (
-					<Text className='text-3xl text-white'>{item.id}</Text>
-				)}
+				renderItem={({ item }) => <VideoCard video={item} />}
 				ListHeaderComponent={() => (
 					<View className='my-6 px-4 space-y-6'>
 						<View className='justify-between items-start flex-row mb-6'>
